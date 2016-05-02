74ddcc44af6328fd451b3db629864544e2af9dfe
garthdb <garthdb@gmail.com>
2016-05-02 17:08:00 -0600

    Added animation to the list items

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var todo = function todo(state, action) {
   switch (action.type) {
@@ -165,32 +175,51 @@ var Todo = function Todo(_ref2) {
     'li',
     {
       onClick: onClick,
-      className: completed ? 'completed' : 'active'
+      className: completed ? 'todo-item-completed' : 'todo-item-active'
     },
     text
   );
 };
 
-var TodoList = function TodoList(_ref3) {
-  var todos = _ref3.todos;
-  var onTodoClick = _ref3.onTodoClick;
-  return React.createElement(
-    'ul',
-    { className: 'todo-list' },
-    todos.map(function (todo) {
-      return React.createElement(Todo, _extends({
-        key: todo.id
-      }, todo, {
-        onClick: function onClick() {
-          return onTodoClick(todo.id);
-        }
-      }));
-    })
-  );
-};
+var TodoList = function (_React$Component) {
+  _inherits(TodoList, _React$Component);
 
-var AddTodo = function AddTodo(_ref4) {
-  var dispatch = _ref4.dispatch;
+  function TodoList(props) {
+    _classCallCheck(this, TodoList);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(TodoList).call(this, props));
+  }
+
+  _createClass(TodoList, [{
+    key: 'render',
+    value: function render() {
+      var _this2 = this;
+
+      return React.createElement(
+        'ul',
+        { className: 'todo-list' },
+        React.createElement(
+          ReactCSSTransitionGroup,
+          { transitionName: 'todo-transition', transitionEnterTimeout: 50, transitionLeaveTimeout: 50 },
+          this.props.todos.map(function (todo) {
+            return React.createElement(Todo, _extends({
+              key: todo.id
+            }, todo, {
+              onClick: function onClick() {
+                return _this2.props.onTodoClick(todo.id);
+              }
+            }));
+          })
+        )
+      );
+    }
+  }]);
+
+  return TodoList;
+}(React.Component);
+
+var AddTodo = function AddTodo(_ref3) {
+  var dispatch = _ref3.dispatch;
 
   var input = void 0;
 
